e25c8d69ef0ff8499e3f9d97326a4f9a99ef637a
Aayush <aayush@ksolves.com>
2021-06-18 15:43:23 +0530

    Second Commit

---
+++ Sources/KColors.swift
@@ -0,0 +1,13 @@
+//
+//  KColors.swift
+//  FirstCode
+//
+//  Created by Ksolves-Macbook-Pro-001 on 18/06/21.
+//
+
+import Foundation
+import UIKit
+
+struct KColors {
+    var perfect: UIColor = .red
+}
